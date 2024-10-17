bf2cc4e32c92080e52d0dcd5582b53e09e1585b1
zcabeto <zcabeto@ucl.ac.uk>
2024-10-17 16:18:10 +0100

    standardise text

--- dataset/text_proc.py
+++ dataset/text_proc.py
@@ -1,7 +1,26 @@
-### IMPORTS ###
+### IMPORTS ### 
+import re
 
 ### LARGE MANAGER ###
 def cleanText(text):
+    text = standardise(text)
+    text = expand_abbrevs(text)
     return text
 
-### INDIVIDUAL CHANGES ###
+### STANDARDISE FORMATTING ###
+def cut_whitespace(text):
+    return re.sub(r'\s+', ' ', text.strip())
+
+def remove_punctuation(text):
+    return re.sub("['`!(),.:;?\"]", '', text)
+
+def lower_case(text):
+    return text.lower()
+
+def standardise(text):
+    text = cut_whitespace(text)
+    text = remove_punctuation(text)
+    text = lower_case(text)
+    return text
+
+def expand_abbrevs(text):    
