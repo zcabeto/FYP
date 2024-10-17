80e78f42e72077ccc2fa3679a437b43cc0bd3cb9
zcabeto <zcabeto@ucl.ac.uk>
2024-10-17 16:19:11 +0100

    expand abbreviations

--- dataset/text_proc.py
+++ dataset/text_proc.py
@@ -23,4 +23,34 @@ def standardise(text):
     text = lower_case(text)
     return text
 
+### EXPAND ABBREVIATIONS ###
+abbrev = {'mrs' :   'misses',
+        'mr'    :   'mister',
+        'dr'    :   'doctor',
+        'st'    :   'saint',
+        'co'    :   'company',
+        'jr'    :   'junior',
+        'maj'   :   'major',
+        'gen'   :   'general',
+        'drs'   :   'doctors',
+        'rev'   :   'reverend',
+        'lt'    :   'lieutenant',
+        'hon'   :   'honorable',
+        'sgt'   :   'sergeant',
+        'capt'  :   'captain',
+        'esq'   :   'esquire',
+        'ltd'   :   'limited',
+        'col'   :   'colonel',
+        'ft'    :   'fort'
+}
+
 def expand_abbrevs(text):    
+    # use regex to make pattern to match with
+    pattern = re.compile(r'\b(' + '|'.join(re.escape(key) for key in abbrev.keys()) + r')\b', re.IGNORECASE)
+    # function for replacing the found matches
+    def replace(match):
+        abbrev_key = match.group(0)
+        expansion = abbrev[abbrev_key]
+        return expansion
+    # apply the function with created pattern and replacement
+    return pattern.sub(replace, text)
