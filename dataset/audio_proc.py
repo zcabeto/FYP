1740c91bde8121f6f496cd76fa94aeb479598231
zcabeto <zcabeto@ucl.ac.uk>
2024-10-18 18:18:35 +0100

    clean audio

--- dataset/audio_proc.py
+++ dataset/audio_proc.py
@@ -6,47 +6,51 @@ import librosa as lb
 import numpy as np
 
 
-### EXTRACT AUDIO DATA ###
+### OVERALL MANAGER ###
+def getFeatures(filepath):
+    raw_audio, sr = lb.load(filepath, sr=None)
+    process_audio = processAudio(raw_audio)
+    audio_features = buildFeatures(process_audio)
+    playable_audio = format_audio(process_audio)
+    return audio_features, playable_audio, sr
+
+### GENERAL AUDIO CLEANING ###
+def processAudio(audio):
+    audio = trim_edges(audio)
+    audio = cut_pauses(audio)
+    return audio
+
+def trim_edges(audio, top_db=40):
+    audio, i = lb.effects.trim(audio, top_db=top_db)
+    return audio
+
+def cut_pauses(audio, top_db=40, hop_length=1000):
+    audio_parts_ranges = lb.effects.split(audio,top_db=top_db, hop_length=hop_length)
+    segments = []
+    for start, end in audio_parts_ranges:
+        segment = audio[start:end]
+        segments.append(segment)
+    restitched = np.concatenate(segments)
+    return restitched
+
+### PREPARE TO PLAY ###
 def format_audio(data):
     return np.int16(data / np.max(np.abs(data)) * 32767)
-    
-def extractAudioData(item):
-    unformatted_data, samplerate = lb.load(item.file, sr=None)
-    unformatted_data, index = lb.effects.trim(unformatted_data, top_db=40)
-    unformatted_data, samplerate = processAudio(unformatted_data, samplerate)
-    item.setAudio(format_audio(unformatted_data), samplerate)
 
-### LARGE MANAGER ###
-def processAudio(audio, sr):
-    return audio, sr
-    #data = lb.effects.percussive(lb.effects.pitch_shift(data, sr=samplerate, n_steps=4))
+### PREPARE FEATURES ###
+def buildFeatures(audio):
+    spectrogram = make_spectrogram(audio)
+    spectrogram = normalise_and_scale(spectrogram)
+    return spectrogram
 
-### INDIVIDUAL EFFECTS ###
+def make_spectrogram(audio, mel=True):
+    spectrogram = lb.stft(audio)
+    # convert to mel spectrogram
+    return spectrogram
 
+### NORMALISATION & SCALING ###
+def normalise_and_scale(audio):
+    return audio
 
-
-### unused ###
-def cut_at_pauses(text, audio):
-    #text = re.sub('["~@£$%^*]','',re.sub("'",'',re.sub('-',' ',text))).lower()
-    #text_parts = re.split('[,;:]',text)
-    text_parts = text.split(',')
-    if text_parts[-1] == '': text_parts = text_parts[:-1]
-    if text_parts[0] == '': text_parts = text_parts[1:]
-    #print(len(text_parts))
-    if (len(text_parts) == 1): return [text], [audio]
-    audio_parts_ranges = []
-    top_db = 20
-    max_db = 40
-    while top_db <= max_db:
-        audio_parts_ranges = lb.effects.split(audio,top_db=top_db, hop_length=4000)
-        #print(top_db, len(audio_parts_ranges))
-        if len(text_parts) == len(audio_parts_ranges):
-            break
-        top_db += 5
-    if len(text_parts) != len(audio_parts_ranges): return [text], [audio]
-    audio_parts = []
-    for i in range(len(audio_parts_ranges)):
-        audio_parts.append(audio[audio_parts_ranges[i,0] : audio_parts_ranges[i,1]])
-        text_parts[i] = text_parts[i].strip()
-    print(top_db)
-    return text_parts, audio_parts   
+def amplitude_to_db(audio):
+    return audio 
