f6d2080d7e96c072b9e255d598d71004ad96dfa8
zcabeto <zcabeto@ucl.ac.uk>
2024-10-18 22:08:54 +0100

    progress counter

--- dataset/extraction.py
+++ dataset/extraction.py
@@ -2,6 +2,8 @@
 from pathlib import Path
 import audio_proc as AudioProcess
 import text_proc as TextProcess
+import time
+import datetime
 
 
 missing_ids = {}
@@ -44,7 +46,7 @@ def nextData(destination, lineNo):
     if audioFile.is_file():
         item = destination.addItem(fileId)
         raw_audio, playable_audio, sr = AudioProcess.getAudio(audioFile)
-        audioFeatures = AudioProcess.getFeatures(raw_audio)
+        audioFeatures = AudioProcess.getFeatures(raw_audio, sr)
         textFeatures = TextProcess.getFeatures(text)
         item.setData(text, playable_audio, sr)
         item.setFeatures(textFeatures, audioFeatures)
@@ -54,11 +56,20 @@ def nextData(destination, lineNo):
     # return to allow looping
     return lineNo
 
-def extractData(destination, limit=None):
-    total_count = 0
+def extractData(destination, limit=13100):
+    count = 0
     lineNo = 1
+    start_time = time.time()
     while lineNo:
         lineNo=nextData(destination, lineNo)
-        total_count += 1
-        if limit: 
-            if total_count > limit: break
+        if progress_tracker(count, limit, start_time): break
+        count += 1
+
+def progress_tracker(count, limit, start_time):
+    now_fract = (count / limit)
+    last_fract = ((count-1) / limit)
+    if int(now_fract * 10) > int(last_fract * 10):
+        message = ("Extraction {percent}% complete ({count}/{limit}). Run for {time}.").format(percent=int(now_fract*10)*10, count=count, limit=limit, time=datetime.timedelta(seconds=(time.time()-start_time)))
+        print(message)
+    if count >= limit: return True
+    else: return False
