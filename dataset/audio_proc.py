fd037a86edf977fb9b5caca05fe6b5fd25417a0c
zcabeto <zcabeto@ucl.ac.uk>
2024-10-20 13:25:12 +0100

    reconstruction methods
    created functionality to reconstruct audio from mel spectrogram, data normalised and scaled more appropriately, progress tracker improved

--- dataset/audio_proc.py
+++ dataset/audio_proc.py
@@ -6,12 +6,13 @@ import librosa as lb
 import numpy as np
 
 
+sr = 22050
+
 ### OVERALL MANAGER ###
 def getAudio(filepath):
     raw_audio, sr = lb.load(filepath, sr=None)
     processed_audio = processAudio(raw_audio)
-    playable_audio = format_audio(processed_audio)
-    return processed_audio, playable_audio, sr
+    return processed_audio, sr
 
 ### GENERAL AUDIO CLEANING ###
 def processAudio(audio):
@@ -32,27 +33,67 @@ def cut_pauses(audio, top_db=40, hop_length=1000):
     restitched = np.concatenate(segments)
     return restitched
 
-### PREPARE TO PLAY ###
-def format_audio(data):
-    return np.int16(data / np.max(np.abs(data)) * 32767)
-
 ### PREPARE FEATURES ###
-def getFeatures(audio, sr):
-    spectrogram = make_spectrogram(audio, sr, mel=True)
-    spectrogram = normalise_and_scale(spectrogram)
+def getFeatures(audio, sr=22050, mel=True, n_mels=256, n_fft=1024, hop_length=256):
+    if mel:
+        spectrogram = lb.feature.melspectrogram(
+            y=audio,
+            sr=sr,
+            n_fft=n_fft,
+            hop_length=hop_length,
+            n_mels=n_mels,
+            power=2.0
+        )
+    else:
+        spectrogram = lb.stft(y=audio, n_fft=n_fft, hop_length=hop_length)
+        spectrogram = np.abs(spectrogram) ** 2
     return spectrogram
 
-def make_spectrogram(audio, sr, mel=True, n_mels=80):
-    spectrogram = lb.stft(audio)
-    if mel:
-        return lb.feature.melspectrogram(S=np.abs(spectrogram), sr=sr, n_mels=n_mels)
-    else:
-        return spectrogram
-
-def normalise_and_scale(spectrogram):
-    mean = np.mean(spectrogram)
-    std = np.std(spectrogram)
-    max_vol = mean + 2*std
-    spectrogram_scaled = np.clip(spectrogram, 0, max_vol)
-    spectrogram_normalised = (spectrogram_scaled - mean) / std
+def normalise_and_scale(spectrogram, mean, stdev):
+    spectrogram = lb.power_to_db(spectrogram, ref=1.0)
+    spectrogram_normalised = (spectrogram - mean) / stdev
     return spectrogram_normalised
+
+### REVERSAL TO PLAY ###
+def play_mel_spectrogram(spectrogram, mean, stdev, sr=22050):
+    spectrogram = denormalise(spectrogram, mean, stdev)
+    spectrogram = mel_to_linear(spectrogram)
+    audio = reconstruct_waveform(spectrogram)
+    playGeneric(audio, sr=sr)
+
+def reconstruct_waveform(linear_spectrogram, n_fft=1024, hop_length=256, win_length=None, window='hann', n_iter=200):
+    waveform = lb.griffinlim(
+        linear_spectrogram,
+        n_iter=n_iter,
+        hop_length=hop_length,
+        win_length=win_length,
+        window=window,
+        n_fft=n_fft
+    )
+    return waveform
+
+def mel_to_linear(mel_spectrogram, sr=22050, n_fft=1024):
+    linear_spectrogram = lb.feature.inverse.mel_to_stft(
+        M=mel_spectrogram,
+        sr=sr,
+        n_fft=n_fft,
+        power=2.0
+    )
+    return linear_spectrogram
+
+
+def denormalise(spectrogram, mean, stdev):
+    spectrogram = (spectrogram * stdev) + mean
+    spectrogram = lb.db_to_power(spectrogram, ref=1.0)
+    return spectrogram
+
+def playGeneric(audio, sr=22050):
+    audio = np.int16(audio / np.max(np.abs(audio)) * 32767)
+    p = pa.PyAudio()
+    try:
+        stream = p.open(format=pa.paInt16, channels=1, rate=sr, output=True)
+        stream.write(audio.tobytes())
+        stream.stop_stream()
+        stream.close()
+    finally:
+        p.terminate()
--- dataset/extraction.py
+++ dataset/extraction.py
@@ -1,10 +1,11 @@
 ### IMPORTS ###
 from pathlib import Path
-import audio_proc as AudioProcess
-import text_proc as TextProcess
+from . import audio_proc as AudioProcess
+from . import text_proc as TextProcess
+import numpy as np
 import time
 import datetime
-
+from . import dataset as ds
 
 missing_ids = {}
 route = './LJSpeech-1.1/wavs/'
@@ -23,7 +24,7 @@ def fileFromId(id):
 
 
 ### ITERATE THROUGH DATA ###
-def nextData(destination, lineNo):
+def nextData(destination, lineNo, total_audio):
     # get the line data
     line = audio_texts.readline()
     if not line: return False
@@ -44,32 +45,48 @@ def nextData(destination, lineNo):
     text = line_parts[2].replace('\n','')
     audioFile = Path(route + fileId + '.wav')
     if audioFile.is_file():
-        item = destination.addItem(fileId)
-        raw_audio, playable_audio, sr = AudioProcess.getAudio(audioFile)
+        item = ds.TTS_Item(fileId)
+        raw_audio, sr = AudioProcess.getAudio(audioFile)
         audioFeatures = AudioProcess.getFeatures(raw_audio, sr)
+        total_audio = np.concatenate((total_audio, audioFeatures.flatten()))
         textFeatures = TextProcess.getFeatures(text)
-        item.setData(text, playable_audio, sr)
+        item.setData(text, raw_audio, sr)
         item.setFeatures(textFeatures, audioFeatures)
+        destination.addItem(item)
     else:
         missing_ids[line_parts[0]] = None
     
     # return to allow looping
-    return lineNo
+    return lineNo, total_audio
 
 def extractData(destination, limit=13100):
+    last_min = 0
     count = 0
     lineNo = 1
+    total_audio = np.array([])
     start_time = time.time()
+    print('Extracting data')
     while lineNo:
-        lineNo=nextData(destination, lineNo)
-        if progress_tracker(count, limit, start_time): break
+        lineNo, total_audio = nextData(destination, lineNo, total_audio)
+        if progress_tracker(count, limit, start_time, last_min): break
         count += 1
+    print('Normalising & Scaling Data')
+    mean = np.mean(total_audio)
+    stdev = np.std(total_audio)
+    print(mean, stdev)
+    destination.setStats(mean, stdev)
+    for item in destination.setlist:
+        textFeature, audioFeature = item.getFeatures()
+        normalised_audio = AudioProcess.normalise_and_scale(audioFeature, mean=mean, stdev=stdev)
+        item.setFeatures(textFeature, normalised_audio)
+    
 
-def progress_tracker(count, limit, start_time):
-    now_fract = (count / limit)
-    last_fract = ((count-1) / limit)
-    if int(now_fract * 10) > int(last_fract * 10):
-        message = ("Extraction {percent}% complete ({count}/{limit}). Run for {time}.").format(percent=int(now_fract*10)*10, count=count, limit=limit, time=datetime.timedelta(seconds=(time.time()-start_time)))
-        print(message)
+def progress_tracker(count, limit, start_time, last_min):
+    percent = (count / limit) * 100
+    time_passed = datetime.timedelta(seconds=(time.time()-start_time))
+    minutes_passed = int(time_passed.total_seconds() // 60)
+    if int(minutes_passed) > last_min:
+        last_min += time_passed.min
+        print(f"Extraction {percent}% complete ({count}/{limit}). Run for {time_passed}.")
     if count >= limit: return True
     else: return False
