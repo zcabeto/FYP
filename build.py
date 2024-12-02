dc564287b08dbb4f6515e6df3cc9dd3b0c3ad361
zcabeto <zcabeto@ucl.ac.uk>
2024-12-02 10:50:53 +0000

    build w/GPU

--- build.py
+++ build.py
@@ -1,4 +1,3 @@
-import sounddevice as sd
 import torch
 from torch.nn.utils.rnn import pad_sequence
 from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -6,30 +5,40 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import model as NN
+import time
+import datetime
+import matplotlib.pyplot as plt
+from librosa import display
+import numpy as np
+from data_create import text_proc as TextProcessor
 from data_create import dataset as ds
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def getData(num_mels, batch_size):
-    dataset = ds.setup(num_mels, limit=100)
+device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.mps.is_available() else 'cpu')
+
+def getData(num_mels, batch_size, n):
+    dataset = ds.setup(num_mels, limit=n)
     train_dataset, val_dataset, test_dataset = dataset.split_sets(val=10)
-    #print(train_dataset.__len__(), val_dataset.__len__(), test_dataset.__len__())
+        
     train_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
         shuffle=False,
-        collate_fn=collate_fn
+        collate_fn=collate_fn,
+        num_workers=0
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=batch_size,
         shuffle=False,
-        collate_fn=collate_fn
+        collate_fn=collate_fn,
+        num_workers=0
     )
     test_loader = DataLoader(
         test_dataset,
         batch_size=batch_size,
         shuffle=False,
-        collate_fn=collate_fn
+        collate_fn=collate_fn,
+        num_workers=0
     )
     return train_loader, val_loader, test_loader
 
@@ -39,17 +48,17 @@ def getModel(vocab_size, embedding_dim, hidden_dim, num_layers, num_mels, learni
         embedding_dim=embedding_dim,
         encoder_hidden_dim=hidden_dim,
         num_layers=num_layers
-    )
+    ).to(device)
     decoder = NN.Decoder(
         decoder_hidden_dim=hidden_dim,
         encoder_hidden_dim=hidden_dim,
         num_layers=num_layers,
         num_mels=num_mels
-    )
+    ).to(device)
     model = NN.Seq2Seq(encoder, decoder).to(device)
-    criterion = nn.L1Loss()
+    criterion = nn.L1Loss().to(device)
     optimiser = optim.Adam(model.parameters(), lr=learning_rate)
-    return model, criterion, optimiser
+    return model, criterion, optimiser, device
 
 def collate_fn(batch):
     text_inputs, audio_targets = zip(*batch)
@@ -61,20 +70,39 @@ def collate_fn(batch):
     # note that audio must be transposed to get shape (t, n_mels)
     audio_targets = [torch.tensor(a.T, dtype=torch.float) for a in audio_targets]
     audio_lengths = torch.tensor([a.size(0) for a in audio_targets], dtype=torch.long)
-    
+
     # Pad sequences
     text_inputs_padded = pad_sequence(text_inputs, batch_first=True, padding_value=0)
     audio_targets_padded = pad_sequence(audio_targets, batch_first=True, padding_value=0.0)
 
-    return text_inputs_padded, text_lengths, audio_targets_padded, audio_lengths
+    return text_inputs_padded.to(device), text_lengths.to(device), audio_targets_padded.to(device), audio_lengths.to(device)
 
 
-def train(model, device, train_loader, val_loader, criterion, optimiser, num_epochs):
-    #scheduler = ReduceLROnPlateau(optimiser, mode='min', factor=0.5, patience=3, verbose=True)
+def train(model, train_loader, val_loader, criterion, optimiser, num_epochs):
+    scheduler = ReduceLROnPlateau(optimiser, mode='min', factor=0.5, patience=3, verbose=True)
+    torch.backends.cudnn.benchmark = True
+    print(f"Model device: {next(model.parameters()).device}")
+    print(f"Optimiser device: {optimiser.param_groups[0]['params'][0].device}")
+    print(f"Criterion device: {criterion.weight.device if hasattr(criterion, 'weight') else 'N/A'}")
+    textFeatures = TextProcessor.getFeatures("hello world")
+    text_input = torch.tensor(textFeatures, dtype=torch.long).unsqueeze(0).to(device)
     for epoch in range(num_epochs):
+        #generated_spectrogram = model.generate(text_input)
+        #plotFeatures(generated_spectrogram, '../../../../../ug/2021/etomlin/imgs/generated_'+str(epoch), save=True)
+        '''print(f"Epoch {epoch} parameters...")
+        for name, param in model.named_parameters():
+            if param.requires_grad:
+                print(f"{name}: mean={param.data.mean().item():.4f}, std={param.data.std().item():.4f}")'''
+        start_time = time.time()
         model.train()
         total_loss = 0
         for batch_idx, (text_inputs, text_lengths, audio_targets, audio_lengths) in enumerate(train_loader):
+            '''if batch_idx == 0:
+                for item in range(len(audio_targets)):
+                    audio_target_np = audio_targets[item].detach().cpu().numpy()
+                    #audio_pca_target_np = audio_pca_targets[item].detach().cpu().numpy()
+                    plotFeatures(audio_target_np, '../../../../../ug/2021/etomlin/imgs/normal_'+str(item), save=True)
+                    #plotFeatures(audio_pca_target_np, '../../../../../ug/2021/etomlin/imgs/pca_'+str(item), save=True)'''
             # move data to device
             text_inputs = text_inputs.to(device)
             text_lengths = text_lengths.to(device)
@@ -99,9 +127,10 @@ def train(model, device, train_loader, val_loader, criterion, optimiser, num_epo
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
             optimiser.step()
             total_loss += loss.item()
-        
+
         avg_loss = total_loss / len(train_loader)
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}')
+        time_passed = datetime.timedelta(seconds=(time.time() - start_time))
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}, Time: {time_passed}')
 
         # step the lr with quick validation
         model.eval()
@@ -115,12 +144,12 @@ def train(model, device, train_loader, val_loader, criterion, optimiser, num_epo
                 outputs = model(text_val_inputs, text_val_lengths, audio_val_targets, audio_val_lengths)
                 loss = criterion(outputs, audio_val_targets)
                 validation_loss += loss.item()
-        
+
         avg_validation_loss = validation_loss / len(val_loader)
         print(f"Validation Loss: {avg_validation_loss:.4f}")
-        #scheduler.step(avg_validation_loss)
+        scheduler.step(avg_validation_loss)
 
-def test(model, device, test_loader, criterion, optimiser):
+def test(model, test_loader, criterion, optimiser):
     model.eval()
     test_loss = 0.0
     with torch.no_grad():  # disable gradients
