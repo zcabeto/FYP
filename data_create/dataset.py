27ae4a619f4b0e844a98e9463e0a9b979d510f58
zcabeto <zcabeto@ucl.ac.uk>
2024-12-11 09:14:53 +0000

    Update dataset.py

--- data_create/dataset.py
+++ data_create/dataset.py
@@ -2,14 +2,11 @@
 from . import extraction
 from torch.utils.data import Dataset
 
-
-### OVERALL DATASET ###
+### DATASET CLASSES ###
 class TTS_Dataset(Dataset):
     def __init__(self):
-        self.set = {}
         self.setlist = []
-        self.mean = 0.0
-        self.stdev = 1.0
+
     def addItem(self, item):
         self.setlist.append(item)
 
@@ -35,49 +32,21 @@ class TTS_Dataset(Dataset):
             index += 1
         return train_set, val_set, test_set
 
-    def setStats(self, mean, stdev):
-        self.mean = mean
-        self.stdev = stdev
-
-### EACH ITEM SPLIT UP ###
 class TTS_Item:
     def __init__(self, id):
         self.id = id
-        self.filepath = fileFromId(id)
-
-        self.textFeature = None     # numpy array
-        self.audioFeature = None    # numpy array
-        self.text = None
-        self.audio = None
-        self.sr = None
+        self.textFeature = None
+        self.audioFeature = None
 
     def setFeatures(self, textFeature, audioFeature):
         self.textFeature = textFeature
         self.audioFeature = audioFeature
 
     def getFeatures(self):
-        return self.textFeature, self.audioFeature#, self.audio
+        return self.textFeature, self.audioFeature
 
-    def setData(self, text, audio, sr):
-        self.text = text
-        self.audio = audio
-        self.sr = sr
-
-    def getData(self):
-        return self.text, self.audio, self.sr
-
-### AUXILLIARY FUNCTIONS ###
-def fileFromId(id):
-    return route + id + '.wav'
-
-### RUNNABLE ###
-missing_ids = {}
+### SETUP DATASET ###
 dataset = TTS_Dataset()
-from pathlib import Path
-root_dir = str(Path(__file__).resolve().parent.parent.parent)
-audio_texts = open(root_dir+"/LJSpeech-1.1/metadata.csv", 'r')
-route = root_dir + "/LJSpeech-1.1/wavs/"
-
 def setup(hparams, use_existing_data=False):
     extraction.extractData(dataset, hparams, use_existing_data=use_existing_data)
     return dataset
