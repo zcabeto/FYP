a5a68642635c5a3ab62bb10386969e0320c46f9e
zcabeto <zcabeto@ucl.ac.uk>
2025-05-23 12:19:48 +0100

    re move around

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 source spectrogram_generation/TTS_Venv/bin/activate
 
 # run the python script for generating spectrograms
-python spectrogram_generation/TextToSpec/run.py -o spectrogram_generation/spectrograms -c spectrogram_generation/model.pt -t inputs.tsv
+python spectrogram_generation/TTS/run.py -o spectrogram_generation/spectrograms -c spectrogram_generation/model.pt -t inputs.tsv
 
 # activate the venv_waveglow virtual environment
 deactivate
